44291ec70e5e3b4d9f4fa93ab03c8626f228b01a
casperfg <casperfg@stud.ntnu.no>
2022-12-18 00:31:16 +0100

    Improved randomCoordinates function

--- src/assets/InitialImages.jsx
+++ src/assets/InitialImages.jsx
@@ -4,10 +4,13 @@ for (let i = 1; i <= 16; i++) {
 	images.push(require(`../Images/viking/image${i}.png`));
 }
 
-const getRandomCoordinates = () => ({
-	x: Math.random() * 1000,
-	y: Math.random() * 700,
-});
+function getRandomCoordinates(padding = 80) {
+	const { innerWidth, innerHeight } = window;
+	return {
+		x: Math.random() * (innerWidth - padding * 2) + padding,
+		y: Math.random() * (innerHeight - padding * 2) + padding,
+	};
+}
 
 const initialImages = images.map((image, i) => ({
 	...getRandomCoordinates(),
