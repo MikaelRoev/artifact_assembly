c203c93517644dc7bf1fdff0fd57b9393d39edfe
casperfg <casperfg@stud.ntnu.no>
2022-12-18 00:25:38 +0100

    Refactoring for readability

--- src/assets/InitialImages.jsx
+++ src/assets/InitialImages.jsx
@@ -4,9 +4,13 @@ for (let i = 1; i <= 16; i++) {
 	images.push(require(`../Images/viking/image${i}.png`));
 }
 
-const initialImages = images.map((image, i) => ({
+const getRandomCoordinates = () => ({
 	x: Math.random() * 1000,
 	y: Math.random() * 700,
+});
+
+const initialImages = images.map((image, i) => ({
+	...getRandomCoordinates(),
 	id: `image${i + 1}`,
 	imageUrl: image,
 }));
